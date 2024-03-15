155d0770ef7c78243c41d734aec1bb3ea749db72
SachaNevsky <sacha.nevsky@gmail.com>
2024-03-15 12:28:16 +0100

    Added background noise

---
+++ app/background/page.jsx
@@ -0,0 +1,63 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faRefresh, faArrowRight, faArrowLeft, faPause } from "@fortawesome/free-solid-svg-icons";
+
+const Background = () => {
+
+    const videoRef = useRef(null);
+    const vocalRef = useRef(null);
+    const carRef = useRef(null);
+
+    const [backgroundVolume, setBackgroundVolume] = useState(20);
+    const [vocalVolume, setVocalVolume] = useState(100);
+
+	const handlePlay = () => {
+		videoRef.current.play();
+        vocalRef.current.play();
+        carRef.current.play();
+	}
+
+	const handlePause = () => {
+		videoRef.current.pause();
+        vocalRef.current.pause();
+        carRef.current.pause();
+	}
+
+	const handleRestart = () => {
+		videoRef.current.currentTime = 0;
+		videoRef.current.pause();
+	}
+
+    useEffect(() => {
+        if(!!vocalRef.current || !!carRef.current || !!videoRef.current) {
+            vocalRef.current.volume = vocalVolume/100;
+            carRef.current.volume = backgroundVolume/100;
+        }
+
+    }, [vocalVolume, backgroundVolume])
+
+    return(
+        <div className="bg-black p-4 h-screen text-white text-center">
+			<div className="grid grid-cols-9 pb-2">
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900 col-start-4" onClick={handlePlay}>Play <FontAwesomeIcon icon={faPlay} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900" onClick={handlePause}>Pause <FontAwesomeIcon icon={faPause} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900" onClick={handleRestart}>Restart <FontAwesomeIcon icon={faArrowLeft} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+			</div>
+			<video id="video" src="/bbc_video.mp4" ref={videoRef} type="video/mp4" className="mx-auto h-4/5" />
+            <audio src="bbc_vocals.mp3" type="audio/mpeg" ref={vocalRef}/>
+            <audio src="car_audio.mp3" type="audio/mpeg" ref={carRef}/>
+            <div className="grid grid-cols-10 gap-1 text-left pt-2">
+                <label className="col-start-5">Vocal Volume: </label>
+                <input type="range" id="vocalSlider" name="Vocals" min="0" max="100" value={vocalVolume} onChange={(e) => setVocalVolume(e.target.value)}/>
+            </div>
+            <div className="grid grid-cols-10 gap-1 text-left py-2">
+                <label className="col-start-5">Background Volume: </label>
+                <input type="range" id="backgroundSlider" name="Background" min="0" max="100" value={backgroundVolume*5} onChange={(e) => setBackgroundVolume(e.target.value/5)}/>
+            </div>
+		</div>
+    )
+}
+
+export default Background;
