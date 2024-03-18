f4d099529ccafaf84595c722309a9f5eb667e9e3
SachaNevsky <sacha.nevsky@gmail.com>
2024-03-18 14:25:59 +0100

    Added complexity marker

---
+++ app/complex_marker/page.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import { useEffect, useState, useRef } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay, faRefresh, faArrowRight, faArrowLeft, faPause } from "@fortawesome/free-solid-svg-icons";
+
+const Home = () => {
+	const [index, setIndex] = useState(1);
+
+	const videoRef = useRef(null);
+
+	const handlePlay = () => {
+		videoRef.current.play();
+	}
+
+	const handlePause = () => {
+		videoRef.current.pause();
+	}
+
+	const handleRestart = () => {
+		videoRef.current.currentTime = 0;
+		videoRef.current.pause();
+	}
+
+	return (
+		<div className="bg-black p-4 h-[96vh] text-white text-center">
+			<div className="grid grid-cols-9">
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900 col-start-4" onClick={handlePlay}>Play <FontAwesomeIcon icon={faPlay} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900" onClick={handlePause}>Pause <FontAwesomeIcon icon={faPause} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+				<button className="bg-gray-700 py-2 px-3 mx-5 rounded-lg hover:bg-gray-900" onClick={handleRestart}>Restart <FontAwesomeIcon icon={faArrowLeft} style={{"padding": "0 0.5vw 0 0.5vw"}}/></button>
+			</div>
+			<video id="video" src="/complexity_marker.mp4" ref={videoRef} type="video/mp4" className="mx-auto h-5/6" />
+		</div>
+	);
+}
+
+export default Home;
